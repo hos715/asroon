c1e1acd244d3d2f8ab7f7a8ef78edbd0d33ee03b
HosseinKhalili <45583045+hos715@users.noreply.github.com>
2021-12-08 10:50:25 +0330

    first config for init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="c-app">
+      <div className="c-app__wrapper">
+        <div className="c-app__col">
+            <img className="c-app--image" src="logo.png" width="125" height="132" alt="logo" />
+          <h1>آسرون</h1>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
